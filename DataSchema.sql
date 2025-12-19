1b6aaa56f5a2c61de762c1f8f1503e6c718766f1
teosso <kovalzhis@gmail.com>
2025-12-19 06:38:03 +0200

    Fix indentation for SQLFluff

--- DataSchema.sql
+++ DataSchema.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     user_name VARCHAR(50),
     role_name VARCHAR(20),
     CONSTRAINT users_pk PRIMARY KEY (user_id),
-    -- Розбиваємо довгий рядок для лінтера
+    -- Розбиваємо довгий рядок
     CONSTRAINT user_name_template CHECK (
         user_name ~ '^[A-ZА-Я][a-zа-яА-Яa-zA-Z ]+$'
     )
@@ -27,9 +27,9 @@ CREATE TABLE data_objects (
     data_content TEXT,
     created_at TIMESTAMP,
     CONSTRAINT data_objects_pk PRIMARY KEY (data_id),
-    -- Розбиваємо рядок, який викликав помилку (було 83 символи)
+    -- Виправлено: додав 4 пробіли відступу перед REFERENCES
     CONSTRAINT data_creator_fk FOREIGN KEY (creator_id)
-        REFERENCES users (user_id),
+    REFERENCES users (user_id),
     CONSTRAINT data_name_unique UNIQUE (data_name)
 );
 
@@ -39,7 +39,6 @@ CREATE TABLE access_rights (
     action_rules VARCHAR(255),
     CONSTRAINT rights_pk PRIMARY KEY (right_id),
     CONSTRAINT rights_user_fk FOREIGN KEY (user_id) REFERENCES users (user_id),
-    -- Переносимо регулярний вираз на новий рядок
     CONSTRAINT action_rules_template CHECK (
         action_rules
         ~ '^(READ|WRITE|DELETE|CREATE)(, (READ|WRITE|DELETE|CREATE))*$'
